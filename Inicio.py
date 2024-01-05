acc2180074f925b8432469612f793d993b085560
HernanSan <hernan.alejo.2001@gmail.com>
2024-01-04 21:14:15 -0500

    MOdulo de descarga de pdf

---
+++ Inicio.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_extras.app_logo import add_logo
+from PIL import Image
+import base64
+import io
+
+st.set_page_config(
+    page_title="Inicio",
+    page_icon="🤖",
+    layout= "wide", 
+    initial_sidebar_state='expanded'
+)
+
+file = open("marca UTPL 2018-01.png", "rb")
+contents = file.read()
+img_str = base64.b64encode(contents).decode("utf-8")
+buffer = io.BytesIO()
+file.close()
+img_data = base64.b64decode(img_str)
+img = Image.open(io.BytesIO(img_data))
+resized_img = img.resize((200, 75))  # x, y
+resized_img.save(buffer, format="PNG")
+img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebarNav"] {{
+                background-image: url('data:image/png;base64,{img_b64}');
+                background-repeat: no-repeat;
+                padding-top: 45px;
+                background-position: 40px 50px;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
+st.write("# Welcome to Streamlit! 👋")
+
+#st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
